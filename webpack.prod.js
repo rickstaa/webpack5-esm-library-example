ae460cb5927fe411db74953800d8e12b191c7eb5
rickstaa <rick.staa@outlook.com>
2021-12-22 15:09:59 +0100

    feat: add webpack-bundle-analyzer

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,9 @@
 import { merge } from "webpack-merge";
 import common from "./webpack.common.js";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 export default merge(common, {
   mode: "production",
   devtool: "source-map",
+  plugins: [new BundleAnalyzerPlugin()],
 });
