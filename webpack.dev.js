5bbf0567c311a896a8ba1cbf9b2daafc0193e743
rickstaa <rick.staa@outlook.com>
2021-12-30 11:52:02 +0100

    feat: add webpack dev server

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,4 +4,11 @@ import common from "./webpack.common.js";
 export default merge(common, {
   mode: "development",
   devtool: "inline-source-map",
+  devServer: {
+    static: './dist',
+    hot: true,
+    devMiddleware: {
+      writeToDisk: true
+    }
+  },
 });
